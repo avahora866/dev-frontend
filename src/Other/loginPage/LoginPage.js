cedf6feea9f0576f8b370a45138ef09bfccdbf0d
Abraar Vahora <avahora1@hotmail.com>
2021-02-07 18:34:27 +0000

    sa

--- src/Other/loginPage/LoginPage.js
+++ src/Other/loginPage/LoginPage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 import Navigation from '../Navigation';
-import '../../Navigation.css'
 import './LoginPage.css'
 
 function loginPage() {
