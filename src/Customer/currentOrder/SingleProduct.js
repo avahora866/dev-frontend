f5bec7d0246f9c12cfb5ce4c82cca641e0acea4b
Abraar Vahora <avahora1@hotmail.com>
2021-02-08 20:35:35 +0000

    added the event handeling for the buttons in the customer sections

--- src/Customer/currentOrder/SingleProduct.js
+++ src/Customer/currentOrder/SingleProduct.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import './Order.css'
 
+function decrease() {
+    let num = parseInt(document.getElementById("numOfProd").textContent);
+    if(num == 0){
+        alert("cannot go under 0");
+    }else{
+        document.getElementById("numOfProd").textContent = parseInt(document.getElementById("numOfProd").textContent) - 1;
+    }
+}
+
+function increase() {
+    document.getElementById("numOfProd").textContent = parseInt(document.getElementById("numOfProd").textContent) + 1;
+}
+
 function SingleProduct({product}) {
     return (
         <div>
@@ -8,7 +21,7 @@ function SingleProduct({product}) {
             Name: {product.name} &nbsp;
             Description: {product.description} &nbsp;
             Price: {product.price} &nbsp;   
-            <br /><button>- &nbsp;</button>&nbsp;<textbox className="textarea">0</textbox>&nbsp;<button> &nbsp;+</button><br /><button>Update</button>         
+            <br /><button onClick={decrease}>- &nbsp;</button>&nbsp;<textbox className="textarea" id="numOfProd">0</textbox>&nbsp;<button onClick={increase}> &nbsp;+</button><br /><button>Update</button>         
             </p>    
         </div>
     )
