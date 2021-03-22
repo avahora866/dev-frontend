9f24658bb4e42f31ccfca882cc6fc3c12ae1217b
Abraar Vahora <avahora1@hotmail.com>
2021-03-22 14:23:43 +0000

    Will now clear the session after signing out

--- src/Customer/Navigation.js
+++ src/Customer/Navigation.js
@@ -3,6 +3,20 @@ import '../Navigation.css'
 import {Link} from 'react-router-dom';
 
 function Navigation() {
+
+    function sessionClearAll() {
+        sessionStorage.removeItem('UserId')
+        sessionStorage.removeItem('username')
+        sessionStorage.removeItem('email')
+        sessionStorage.removeItem('fName')
+        sessionStorage.removeItem('lName')
+        sessionStorage.removeItem('dateOfBirth')
+        sessionStorage.removeItem('postcode')
+        sessionStorage.removeItem('area')
+        sessionStorage.removeItem('type')
+    }
+
+
     return (
         <nav >
             <span >
@@ -21,7 +35,7 @@ function Navigation() {
                     <li>Products</li>
                 </Link>
                 <Link to = '/'>
-                    <li>Sign out</li>
+                    <li onClick={sessionClearAll}>Sign out</li>
                 </Link>
                 
             </ul>
