49c1e3b8abb59bcdd44a27ba18794c9f72c13017
Abraar Vahora <avahora1@hotmail.com>
2021-03-22 16:07:42 +0000

    Foun d a problme on the currnetOrder GET request. Need to split the list into two diffrent lists

--- src/Customer/currentOrder/Order.js
+++ src/Customer/currentOrder/Order.js
@@ -1,55 +1,63 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react'
 import SingleProduct from './SingleProduct'
 import Navigation from '../Navigation';
+import axios from 'axios';
 
-
-function Order() {
-    const prod = [
-        {
-            id: 1,
-            name: 'Milk',
-            description: 'To drinkdddddddddddddddddddddddddd',
-            price: 5.47,
-            quaty: 4
-        }, 
-        {
-            id: 2,
-            name: 'Eggs',
-            description: 'Can make omelates withdddddddddddddddddddd',
-            price: 2.99,
-            quaty: 6
-        }, 
-        {
-            id: 3,
-            name: 'Cheese',
-            description: 'I dont knowddddddddddddddddddddddddddd',
-            price: 4.56,
-            quaty: 9
+class Order extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            list : [],
+            totalPrice : 0,
         }
-        ];
-        const prodList = prod.map(product => <SingleProduct key = {product.id} product = {product} />)
-        
-        function calTotalPrice() {
-            let result = 0.00;
-            for (let index = 0; index < prod.length; index++) {
-                result += prod[index].price * prod[index].quaty;
+        // this.calTotalPrice = this.calTotalPrice.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
+    }
+
+    // calTotalPrice() {
+    //     let result = 0.00;
+    //     for (let index = 0; index < this.state.list.length; index++) {
+    //         result += this.state.list[index].price * this.state.list[index].quaty;
+    //     }
+    //     this.setState({totalPrice: result})
+    // }
+
+    splitLists() {
+        let temp = [];
+        for(let index = 0; index < this.state.length/2; index++){
+            
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:9090/milk4u/getCurrentOrder', {
+            params: {
+                id: sessionStorage.getItem('UserId')
             }
-            return result; 
-        }
+        }).then(res => {
+            this.setState({list : res.data});
+          });    
+          
+        //   this.calTotalPrice();
+    }
+    
+    render() {
+        const prodList = this.state.list.map(product => <SingleProduct key = {product.id} product = {product} />)
 
-        const [totalPrice, setTotalPrice] = useState(calTotalPrice);
-   
         return (
-        <div><Navigation />
-        <div >
-            <div >{prodList}</div>
-            <p>
-                <button className="margin">Cancel Order</button>
-                <label className="margin">{totalPrice}</label>
-            </p>
-        </div>
-        </div>
-    )
+            <div><Navigation />
+                <div >
+                    <div >{prodList}</div>
+                    <p>
+                        <button className="margin">Cancel Order</button>
+                        <label className="margin">{this.state.totalPrice}</label>
+                    </p>
+                </div>
+            </div>
+        )
+    }
 }
+
 
 export default Order
--- src/Customer/products/Products.js
+++ src/Customer/products/Products.js
@@ -1,30 +1,26 @@
 import SingleProduct from './SingleProduct'
 import Navigation from '../Navigation';
 import React, { Component } from 'react'
+import axios from 'axios';
+
 
 class Products extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             productList : [],
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:9090/milk4u/getProducts').then(res => {
+            this.setState({productList : res.data});
+          });     
+    }
+
     render() {
-        const prod = [
-            {
-                id: 1,
-                name: 'Milk',
-                description: 'To drinkasassasasas',
-                price: 5.47
-            }, 
-            {
-                id: 2,
-                name: 'Eggs',
-                description: 'Can make omelates with',
-                price: 2.99
-            }, 
-            {
-                id: 3,
-                name: 'Cheese',
-                description: 'I dont knowassasasasdfswgar',
-                price: 4.56
-            }
-            ];
-            const prodList = prod.map(product => <SingleProduct key = {product.id} product = {product} />)
+            const prodList = this.state.productList.map(product => <SingleProduct key = {product.id} product = {product} />)
         return (
             <div><Navigation />
              <div>
